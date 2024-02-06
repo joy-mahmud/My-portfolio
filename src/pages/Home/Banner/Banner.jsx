98656c210e354a61364f5518c4baa077bbf874ac
joy-mahmud <joymahmudiucse@gmail.com>
2024-02-06 10:10:45 +0600

    updated text animation and projects section

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,16 +1,27 @@
 import img from '../../../assets/Joyremovebg-preview.png'
 // import resume from '../../../assets/resume.pdf'
+import { Typewriter } from 'react-simple-typewriter'
 const Banner = () => {
-
-
     return (
         <div className='bg-[#0D1821] w-full flex flex-col-reverse gap-5 md:flex-row items-center justify-between'>
             <div className="">
                 <div className="text-5xl text-white md:pl-20">
                     <h2 className="mb-2">Hi. I am</h2>
-                    <h2>Shakib Ahmed <span className="text-[#DE1945]">Joy</span></h2>
-                    <p className='text-[18px] mt-3 '>
-                        Frontend developer. <br /> Currently working on mern stack development
+                    <h2>Shakib Ahmed <span className="text-[#DE1945] font-bold">JOY</span></h2>
+                    <p className='text-4xl mt-3 '>
+                        I am a  <span style={{ color: '#DE1945', fontWeight: 'bold' }}>
+                            {/* Style will be inherited from the parent element */}
+                            <Typewriter
+                                words={['FRONTEND DEVELOPER','REACT DEVELOPER', 'MERN STACK DEVELOPER']}
+                                loop={1000}
+                                cursor
+                                cursorStyle='|'
+                                typeSpeed={100}
+                                deleteSpeed={80}
+                                delaySpeed={1000}
+                               
+                            />
+                        </span>
                     </p>
                 </div>
                 <div className="md:pl-20 flex justify-center md:justify-start my-10">
--- src/pages/Home/Skill/MyWork/MyWork.jsx
+++ src/pages/Home/Skill/MyWork/MyWork.jsx
@@ -9,17 +9,28 @@ const MyWork = () => {
             <div className="mb-5">
                 <h2 className=" text-center text-[#DE1945] text-4xl font-bold py-2">My Recent projects</h2>
             </div>
-            <div className='grid grid-cols-1 lg:grid-cols-3'>
+            <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
                 <div className='flex flex-col justify-center gap-3 items-center mb-5'>
-                    <img src={foodFun} className="w-[350px] h-[400px] hover:-translate-y-5 duration-500" alt="" />
+                    <div className=' rounded-md '>
+                        
+                            <img src={foodFun} className=" hover:shadow-[#b6b0b2] hover:rounded-md hover:shadow-lg h-[250px] hover:-translate-y-4 duration-500" alt="" />
+                       
+                        <p className='text-justify text-white'>This is the food ordering websites for a hotel where users can add their own food and order foods.compete food ordering functionality like add to cart remove from cart , increasing or decreasing quantities etc.Firebase login registration system.Secure api by jwt with browser cookies.User can book tables for a particular date with a particular number of quests.users can add their own foods and update their details</p>
+                    </div>
                     <Link to='https://foodfun-5c49a.web.app/'><button className="border-2 border-[#DE1945] rounded-md px-3 py-2  text-xl hover:scale-110 duration-300 text-white hover:bg-[#DE1945]">Live Preview</button></Link>
                 </div>
                 <div className='flex flex-col justify-center gap-3 items-center mb-5'>
-                    <img src={parcelJet} className="w-[350px] h-[400px] hover:-translate-y-5 duration-500" alt="" />
+                    <div>
+                        <img src={parcelJet} className="hover:shadow-[#b6b0b2] hover:rounded-md hover:shadow-lg h-[250px] hover:-translate-y-4 duration-500" alt="" />
+                        <p className='text-white'>The complete online parcel delivery management system.There are three different types of user role. The admin role, delivery men role, general user role. They have the different dashboards to perform different tasks.Stripe payment gateway integrated to pay for the delivery.data fetching with tanstack query for better user performance.Different types of filtering system included . filter by date range and other filtering system available.</p>
+                    </div>
                     <Link to='https://parcel-jet.web.app/'><button className="border-2 border-[#DE1945] rounded-md px-3 py-2  text-xl hover:scale-110 duration-300 text-white hover:bg-[#DE1945]">Live Preview</button></Link>
                 </div>
                 <div className='flex flex-col  justify-center gap-3 items-center mb-5'>
-                    <img src={eventCorp} className="w-[350px] h-[400px] hover:-translate-y-5 duration-500" alt="" />
+                    <div>
+                        <img src={eventCorp} className="hover:shadow-[#b6b0b2] hover:rounded-md hover:shadow-lg h-[250px] hover:-translate-y-4 duration-500" alt="" />
+                        <p className='text-white'>This an event management website. In this website firebase authentication system has been used for user login registration. In the home page different types of events are presented as card form. By clicking on details button user can view details of the particular event.There are different routes such as gallary, services,blogs. Users can login and register and the profile pic is shown in the heading section.</p>
+                    </div>
                     <Link to='https://react-event-management-9dca3.web.app'><button className="border-2 border-[#DE1945] rounded-md px-3 py-2 hover:scale-110 duration-300  text-xl text-white hover:bg-[#DE1945]">Live Preview</button></Link>
                 </div>
             </div>
